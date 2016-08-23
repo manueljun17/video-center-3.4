6ee4bd57e0c7f744a52b0ac096022f0a47ffea98
jeraisy21 <thruthesky@gmail.com>
2016-08-23 23:38:12 +0900

    work on server

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 
 
 Video Center Version 3.4
+
+
+# TODO
+
+Hide typescript source code by submodule.
+
+Put typescript submodule in private git server.
+
---
+++ dev/video-center-3.4-server.ts
@@ -0,0 +1,10 @@
+class VideoCenterServer {
+    constructor() {
+        console.log('VideoCenterServer::constructor()');
+    }
+    listen( socket, io ) {
+        console.log('VideoCenterServer::start()');
+    }
+}
+
+exports = VideoCenterServer;
