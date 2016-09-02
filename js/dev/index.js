d0eed036f16506ec71c29e7750414bc4ef50ffa3
thruthesky <thruthesky@gmail.com>
2016-09-02 14:39:24 +0800

    Work on refactoring

--- js/dev/index.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", './chat'], function (require, exports, chat_1) {
-    "use strict";
-    var chat = new chat_1.Chat();
-    chat.start();
-});
-//# sourceMappingURL=index.js.map
--- js/index.js
+++
@@ -1,6 +0,0 @@
-define(["require", "exports", './chat'], function (require, exports, chat_1) {
-    "use strict";
-    var chat = new chat_1.Chat('http://localhost:9001/');
-    chat.start();
-});
-//# sourceMappingURL=index.js.map
