32713da4a2fe8082da75ca69fbeca22c6570180d
ManuelManansala <manuelmanansalajr@gmail.com>
2016-09-21 15:02:39 +0800

    refactor declare

--- dev/declare.ts
+++ dev/declare.ts
@@ -1,4 +1,6 @@
 export const lobbyRoomName:string = 'Lobby';
+export const socket_server_url:string = 'http://dev.withcenter.com:9001/';
+// export const socket_server_url:string = 'http://localhost:9001';
 export interface S {
     ( str: string ) : void;
 }
